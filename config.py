78075cb44ea101582b457b4163d31c9edce76ee2
Daniel Gerardo Rondón García <61068392+DanielRondonGarcia@users.noreply.github.com>
2022-06-23 08:19:44 -0500

    se ajustan pequeños detalles

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-username = 'DRONDON'
-password = 'CC1098809038*'
+username = 'user'
+password = 'password'
 dsn = 'mar.actsis.com'
 port = 1512
 encoding = 'UTF-8'
-apiKey='5618cca12fe41c7d6740979af73aa7c3'
+apiKey='apiKey'
